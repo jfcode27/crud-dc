80a1f065b769390d82b22f9d35eb01f879535026
Roberto Macias <robertomag7@hotmail.com>
2022-11-28 14:14:16 -0600

    Added functionality to assign grades to subject

--- backend/controllers/GradeController.js
+++ backend/controllers/GradeController.js
@@ -17,7 +17,23 @@ const getGradeById = async(req, res) => {
     return res.status(200).json(grade);
 }
 
+const updateGrade = async(req, res) => {
+    const { id } = req.params;
+    const { firstPartial, secondPartial, thirdPartial } = req.body;
+    const finalGrade = (firstPartial + secondPartial + thirdPartial) / 3;
+
+    const grade = await Grade.findOne({ where: { id } });
+    if (!grade) {
+        return res.status(400).json({ message: 'Grade not found.' });
+    }
+
+    const updatedGrade = await Grade.update({ firstPartial, secondPartial, thirdPartial, finalGrade }, { where: { id } });
+
+    return res.status(200).json(updatedGrade);
+}
+
 export {
     getGrades,
-    getGradeById
+    getGradeById,
+    updateGrade
 }
--- backend/routes/GradeRoutes.js
+++ backend/routes/GradeRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import {
     getGrades,
-    getGradeById
+    getGradeById,
+    updateGrade
 } from '../controllers/GradeController.js';
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 // Grades
 router.get('/', getGrades);
 router.get('/:id', getGradeById);
+router.put('/:id', updateGrade);
 
 export default router;
